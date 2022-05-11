30937c9c3c7b457afda1fee7c9fdeb310f53420f
yurimarim <yurimarim@hotmail.com>
2022-05-11 01:11:22 -0300

    Widget Component

---
+++ src/components/Widget/index.tsx
@@ -0,0 +1,47 @@
+import React, { useRef } from 'react';
+import { TouchableOpacity } from 'react-native';
+import { ChatTeardropDots } from 'phosphor-react-native'
+import BottomSheet from '@gorhom/bottom-sheet'
+import { gestureHandlerRootHOC } from 'react-native-gesture-handler'
+
+import { Options } from '../Options';
+import { Form } from '../Form';
+
+
+import { theme } from '../../theme';
+import { styles } from './styles';
+import { feedbackTypes } from '../../utils/feedbackTypes'
+
+export type FeedbackType = keyof typeof feedbackTypes
+
+function Widget() {
+  const bottomSheetRef = useRef<BottomSheet>(null)
+
+  function handleOpen() {
+    bottomSheetRef.current?.expand()
+  }
+
+  return (
+    <>
+      <TouchableOpacity onPress={handleOpen} style={styles.button}>
+        <ChatTeardropDots
+          size={24}
+          weight="bold"
+          color={theme.colors.text_on_brand_color}
+        />
+      </TouchableOpacity>
+      <BottomSheet
+        ref={bottomSheetRef}
+        snapPoints={[1, 280]}
+        backgroundStyle={styles.modal}
+        handleIndicatorStyle={styles.indicator}
+      >
+        <Form
+          feedbackType="BUG"
+        />
+      </BottomSheet>
+    </>
+  );
+}
+
+export default gestureHandlerRootHOC(Widget);
